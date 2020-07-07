5c8048413181bceaa4afedbf4e35264c652b22f1
Stephen Rufle <srufle@gmail.com>
2020-07-07 06:52:43 -0700

    Added Polar Cell

---
+++ src/polar_cell.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'cell'
+
+# PolarCell class used as section Polar Maze
+class PolarCell < Cell
+  attr_accessor :cw, :ccw, :inward
+  attr_reader :outward
+
+  def initialize(_row, _column)
+    super()
+    @outward = []
+  end
+
+  def neighbors
+    list = []
+    list << cw if cw
+    list << ccw if ccw
+    list << inward if inward
+    list += outward
+    list
+  end
+end
