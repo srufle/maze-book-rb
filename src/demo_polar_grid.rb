b4e43a97e36d1196fff066f268897107e54bc441
Stephen Rufle <srufle@gmail.com>
2020-07-08 06:54:43 -0700

    Added more algorithms to polar grid demo

--- src/demo_polar_grid.rb
+++ src/demo_polar_grid.rb
@@ -1,27 +1,25 @@
 # frozen_string_literal: true
 
 require 'polar_grid'
+require 'aldous_broder'
+require 'wilsons'
+require 'hunt_and_kill'
 require 'recursive_backtracker'
 
 now = Time.new
 n = 1
-grid = PolarGrid.new(8)
 
-puts ''
-puts 'Polar Grid %02d - png' % n
+algorithms = [AldousBroder, Wilsons, HuntAndKill, RecursiveBacktracker]
 
-img = grid.to_png
-ts = "#{now.year}-#{now.month}-#{now.day}-#{now.hour}-#{now.min}-#{now.sec}"
-img.save "../img/polar_grid_%02d-#{ts}.png" % n
+algorithms.each do |algorithm|
+  puts "Running #{algorithm} - Polar Grid %02d - png ..." % n
 
-n = 2
-grid = PolarGrid.new(8)
+  grid = PolarGrid.new(16)
+  algorithm.on(grid)
 
-RecursiveBacktracker.on(grid)
+  img = grid.to_png
+  ts = "#{now.year}-#{now.month}-#{now.day}-#{now.hour}-#{now.min}-#{now.sec}"
+  img.save "../img/circle_maze_%02d-#{algorithm}-#{ts}.png" % n
 
-puts ''
-puts 'Polar Grid %02d - Recursive Backtracker - png' % n
-
-img = grid.to_png
-ts = "#{now.year}-#{now.month}-#{now.day}-#{now.hour}-#{now.min}-#{now.sec}"
-img.save "../img/circle_maze_%02d-#{ts}.png" % n
+  n += 1
+end
